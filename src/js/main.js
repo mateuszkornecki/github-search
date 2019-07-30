2d33b25578c5bc2983d2a53f55c0c799d6b784ff
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-07-30 15:40:50 +0200

    fix: fixed 100vh display problem on mobiles

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@
 
 const searchInput = document.querySelector('.search__input--js');
 const profilePage = document.querySelector('.profile--js');
+//! CSS TRICK SOLUTION FOR 100vh on mobiles
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 const profileBuilder = () => {
     profilePage.innerHTML = '';
     searchInput.value = localStorage.getItem('User Name');
